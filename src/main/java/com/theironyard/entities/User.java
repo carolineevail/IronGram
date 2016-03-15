2993fe667d38929ba878eb275ef2b034c96ff708
carolineevail <carolineevail@gmail.com>
2016-03-15 11:52:33 -0400

    Start work on app

---
+++ src/main/java/com/theironyard/entities/User.java
@@ -0,0 +1,44 @@
+package com.theironyard.entities;
+
+import javax.persistence.*;
+
+/**
+ * Created by Caroline on 3/15/16.
+ */
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue
+    int id;
+
+    @Column(nullable = false, unique = true)
+    String name;
+
+    @Column(nullable = false)
+    String passwordHash;
+
+    public User() {
+    }
+
+    public User(String name, String passwordHash) {
+        this.name = name;
+        this.passwordHash = passwordHash;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+}
